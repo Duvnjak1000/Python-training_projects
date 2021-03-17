2873fa70b87df96415329707af8acbb3b6605f20
Duvnjak1000 <igiduv@gmail.com>
2021-03-17 11:25:34 +0100

    x,y renamed

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -5,12 +5,12 @@ second_value= int(input("Enter the second value: "))
 operation = input("Please choose the right arithmetic operation (+, -, *, /): ")
 
 if operation == "+":
-    print(x + y)
+    print(first_value + second_value)
 elif operation == "-":
-    print(x - y)
+    print(first_value - second_value)
 elif operation == "*":
-    print(x * y)
+    print(first_value * second_value)
 elif operation == "/":
-    print(x / y)
+    print(first_value / second_value)
 else:
     print("You did not provide the correct values for operation.")
