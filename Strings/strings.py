5493081e8253094739ac600441c16beaadd212b9
Duvnjak1000 <igiduv@gmail.com>
2021-03-18 19:17:00 +0100

    Notes

--- Strings/strings.py
+++ Strings/strings.py
@@ -2,3 +2,7 @@ str_one = "Happy"
 str_two = "Day!"
 
 print("{0} {1}".format(str_one, str_two))
+
+# we can use the format() method, with placeholders like {0}:
+# print("{0} {1}".format(str_one, str_two))
+    #In programming, counting starts with 0. That's why {0} means the first string.
