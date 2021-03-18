7fd56520a0daee979598048ff9698e64b6e929ea
Duvnjak1000 <igiduv@gmail.com>
2021-03-18 18:47:18 +0100

    Converter mi --> km

---
+++ Unit converter/km-miles_converter.py
@@ -0,0 +1,16 @@
+print("Welcome! This is UC (unit converter) for converting miles into kilometers.")
+
+while True:
+    print("Please enter a number of miles for conversion into kilometers. Enter ONLY a number!")
+
+    mi = input("Miles: ")
+    mi = float(mi.replace(",", ".")) #replace comma with dot, if user enters a comma.
+
+    km = mi * 1.609344
+
+    print("{0} miles is {1} kilometers.".format (mi, km))
+
+    choice = input("Would you like to do another conversion (y/n): ")
+
+    if choice.lower() != "y" and choice.lower() != "yes":
+        break
