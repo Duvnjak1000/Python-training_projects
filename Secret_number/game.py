3c765ae96066f7ae6bef60dff53a9858f32f539a
Duvnjak1000 <igiduv@gmail.com>
2021-03-18 12:54:11 +0100

    imported random secret - FINAL GAME!

--- Secret_number/game.py
+++ Secret_number/game.py
@@ -1,14 +1,14 @@
-secret = 22
+import random
 
+secret = random.randint (1, 30)
 
 while True:
     guess = int(input("Guess the secret number (between 1 and 30): "))
 
     if guess == secret:
-        print("You've guessed it - congratulations! It's number 22.")
+        print("You've guessed it - congratulations! It's number " +str(secret))
         break
     elif guess > secret:
-        print("Sorry, your guess is not correct...Try smaller value than specified " + str(guess))
+        print("Sorry, your guess is not correct...Try smaller value than specified ")
     elif guess < secret:
-        print("Sorry, your guess is not correct...Try higher value than specified " + str (guess))
-
+        print("Sorry, your guess is not correct...Try higher value than specified ")
