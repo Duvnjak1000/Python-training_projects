4577351cf30be63e244b006fd84e5264d8c6d566
Duvnjak1000 <igiduv@gmail.com>
2021-03-18 12:13:39 +0100

    for loop - limited attempts

--- Secret_number/game.py
+++ Secret_number/game.py
@@ -1,10 +1,11 @@
 secret = 22
-guess = 0
 
-while guess != secret:
+
+for x in range(5):
     guess = int(input("Guess the secret number (between 1 and 30): "))
 
     if guess == secret:
         print("You've guessed it - congratulations! It's number 22.")
+        break
     else:
         print("Sorry, your guess is not correct... The secret number is not " + str(guess))
