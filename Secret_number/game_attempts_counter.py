7e7100f859b6ea8275295c8800bf70c34c3e80e0
Duvnjak1000 <igiduv@gmail.com>
2021-03-19 17:23:33 +0100

    comment

--- Secret_number/game_attempts_counter.py
+++ Secret_number/game_attempts_counter.py
@@ -7,6 +7,12 @@ with open("score.txt", "r") as score_file:
     best_score = int(score_file.read())
     print("Top score: " + str(best_score))
 
+    #This will:
+#open the score.txt file,
+#read its contents,
+#save it into the variable named best_score,
+#and print it in the Terminal.
+
 while True:
     guess = int(input("Guess the secret number (between 1 and 30): "))
     attempts += 1
