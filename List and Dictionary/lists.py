76c0e9d2138a84dd0972e11ae7c4d4250ebe5e6f
Duvnjak1000 <igiduv@gmail.com>
2021-03-21 10:03:04 +0100

    sorting

--- List and Dictionary/lists.py
+++ List and Dictionary/lists.py
@@ -1,8 +1,4 @@
 some_list = [1, 34, 12, 17, 87]
+some_list.sort()
 print(some_list)
 
-another_list = ["tesla", "toyota", "nissan"]
-print(another_list)
-
-mixed_list = [22, "elon", True, "SmartNinja", 3.14]
-print(mixed_list)
