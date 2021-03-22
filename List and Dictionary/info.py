11d1d95d3560acbfd8a83bf121cf87c9d6ca4e59
Duvnjak1000 <igiduv@gmail.com>
2021-03-22 12:05:05 +0100

    when to use list/dictionary

---
+++ List and Dictionary/info.py
@@ -0,0 +1,9 @@
+#So when to use list and when to use a dictionary?
+
+#If you want to group data of the same kind, you'd use a list. Example: a list of European cities:
+
+eu_cities = ["Vienna", "Malaga", "Budapest", "Cologne", "Zagreb"]
+
+# But if you'd like to group data of different kinds, you'd use a dictionary:
+
+jordan = {"first_name": "Michael", "last_name": "Jordan", "age": 56, "games_played": 1072, "total_points": 32292}
