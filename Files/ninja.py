6af15d23e897cd16cfdfbe3e96526b961c675563
Duvnjak1000 <igiduv@gmail.com>
2021-06-22 16:06:47 +0200

    deleted projects

--- Files/ninja.py
+++
@@ -1,2 +0,0 @@
-with open("ninja.txt", "w") as ninja_file_2:
-    ninja_file_2.write("Hello, new file ready to write!")
--- Packages/official.py
+++
@@ -1,8 +0,0 @@
-import
-from datetime import datetime
-
-random_num = random.randint(1, 50)
-print(random_num)
-
-time_now = datetime.now()
-print(time_now)
